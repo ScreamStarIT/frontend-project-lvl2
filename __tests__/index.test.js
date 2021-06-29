dc59c239501f2b5fdaadbecb09e7c4484d2b4e02
ScreamStarIT <customsmith@gmail.com>
2021-06-29 15:43:09 +0700

    add fixtures and index.test.js

---
+++ __tests__/index.test.js
@@ -0,0 +1,17 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import { test, expect } from '@jest/globals';
+import genDiff from '../src/index.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
+
+test('genDiff flat JSON', () => {
+  const pathToJSON1 = getFixturePath('file1.json');
+  const pathToJSON2 = getFixturePath('file2.json');
+  const correct = readFile('result.txt');
+  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(correct);
+});
