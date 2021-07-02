99c6518fa4ccec7144a8be70c229a0d56e9b75ee
ScreamStarIT <customsmith@gmail.com>
2021-07-02 15:46:15 +0700

    add genDiff flat YAML test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,6 +12,13 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8')
 test('genDiff flat JSON', () => {
   const pathToJSON1 = getFixturePath('file1.json');
   const pathToJSON2 = getFixturePath('file2.json');
-  const correct = readFile('result.txt');
-  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(correct);
+  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(readFile('result.txt'));
+});
+test('genDiff flat YAML', () => {
+  const pathToYAML1 = getFixturePath('file1.yaml');
+  const pathToYAML2 = getFixturePath('file2.yaml');
+  const pathToYML1 = getFixturePath('file1.yml');
+  const pathToYML2 = getFixturePath('file2.yml');
+  expect(genDiff(pathToYAML1, pathToYAML2)).toBe(readFile('result.txt'));
+  expect(genDiff(pathToYML1, pathToYML2)).toBe(readFile('result.txt'));
 });
