d7c848a0be3e65f8295659a61dbcc1badeb82daa
ScreamStarIT <customsmith@gmail.com>
2021-08-01 16:49:14 +0700

    made new test & add new fixtures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -9,16 +9,13 @@ const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8');
 
-test('genDiff flat JSON', () => {
-  const pathToJSON1 = getFixturePath('file1.json');
-  const pathToJSON2 = getFixturePath('file2.json');
+test('genDiff JSON', () => {
+  const pathToJSON1 = getFixturePath('before.json');
+  const pathToJSON2 = getFixturePath('after.json');
   expect(genDiff(pathToJSON1, pathToJSON2)).toBe(readFile('result.txt'));
 });
-test('genDiff flat YAML', () => {
-  const pathToYAML1 = getFixturePath('file1.yaml');
-  const pathToYAML2 = getFixturePath('file2.yaml');
-  const pathToYML1 = getFixturePath('file1.yml');
-  const pathToYML2 = getFixturePath('file2.yml');
-  expect(genDiff(pathToYAML1, pathToYAML2)).toBe(readFile('result.txt'));
+test('genDiff YAML', () => {
+  const pathToYML1 = getFixturePath('before.yml');
+  const pathToYML2 = getFixturePath('after.yml');
   expect(genDiff(pathToYML1, pathToYML2)).toBe(readFile('result.txt'));
 });
