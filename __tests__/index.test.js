29b319b7e25cef8ff55d8a2b99b1bdc176ddb448
ScreamStarIT <customsmith@gmail.com>
2021-08-03 12:15:54 +0700

    add genDiff json format test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -12,16 +12,22 @@ const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf8')
 test('genDiff JSON', () => {
   const pathToJSON1 = getFixturePath('before.json');
   const pathToJSON2 = getFixturePath('after.json');
-  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(readFile('result.txt'));
+  expect(genDiff(pathToJSON1, pathToJSON2)).toBe(readFile('stylishResult.txt'));
 });
 test('genDiff YAML', () => {
   const pathToYML1 = getFixturePath('before.yml');
   const pathToYML2 = getFixturePath('after.yml');
-  expect(genDiff(pathToYML1, pathToYML2)).toBe(readFile('result.txt'));
+  expect(genDiff(pathToYML1, pathToYML2)).toBe(readFile('stylishResult.txt'));
 });
 
 test('genDiff plain format', () => {
   const pathToFile1 = getFixturePath('before.json');
   const pathToFile2 = getFixturePath('after.json');
-  expect(genDiff(pathToFile1, pathToFile2, 'plain')).toBe(readFile('plain.txt'));
+  expect(genDiff(pathToFile1, pathToFile2, 'plain')).toBe(readFile('plainResult.txt'));
+});
+
+test('genDiff json format', () => {
+  const pathToFile1 = getFixturePath('before.json');
+  const pathToFile2 = getFixturePath('after.json');
+  expect(genDiff(pathToFile1, pathToFile2, 'json')).toBe(readFile('jsonResult.json'));
 });
