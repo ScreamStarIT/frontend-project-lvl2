0b272b8e603d903710cfc32ec578f62afc6168e7
ScreamStarIT <customsmith@gmail.com>
2021-08-04 19:03:22 +0700

    update version

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,7 +6,7 @@ import genDiff from '../index.js';
 const program = new Command();
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('0.4.0')
+  .version('1.0.0')
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format', 'stylish')
   .action(
